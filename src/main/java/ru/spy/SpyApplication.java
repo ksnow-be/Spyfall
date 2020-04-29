1532bbff0a495856bcd0c8416ea82221fb410746
Чернов Олег Николаевич <Chernov.O.N@sberbank.ru>
2020-04-29 22:07:08 +0300

    added project

---
+++ src/main/java/ru/spy/SpyApplication.java
@@ -0,0 +1,13 @@
+package ru.spy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spy/SpyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
